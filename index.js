2cbec16972ffd1c021be1ad16ccb331ef6eb867f
hhiirraa <162493836+hhiirraa@users.noreply.github.com>
2024-05-22 00:40:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var greetVisitor="Hello! Welcome to our website!"
+    alert(greetVisitor);
+    var javaScriptAlert
+    alert("Error! please enter a valid password ")
+    var javaScriptAlert="Welcome to JS Land...\n Happy coding!"
+    alert( javaScriptAlert) 
+    var javaScriptAlert=" WElcome to js land\v Happy coding... "  
+     alert( javaScriptAlert);
+
+
+    
+     console.log("Message 1");
+
+     setTimeout(function() {
+       console.log("Message 2");
+     }, 2000); // 2000 milliseconds = 2 seconds
+     
+     setTimeout(function() {
+       console.log("Message 3");
+     }, 4000); // 4000 milliseconds = 4 seconds
+
+     function showAlert() {
+        alert("Hello, World!");
+    }
